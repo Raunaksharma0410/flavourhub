104bee46c18da247e6b9288a5aed01420e1a16dc
Raunak sharma <iamroni98177@gmail.com>
2026-08-13 09:19:36 +0530

    Add automatic production superuser setup

--- build.sh
+++ build.sh
@@ -1,5 +1,44 @@
 #!/usr/bin/env bash
 set -o errexit
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
+
 python manage.py migrate
+
+if [ -n "${DJANGO_SUPERUSER_USERNAME:-}" ] && [ -n "${DJANGO_SUPERUSER_EMAIL:-}" ] && [ -n "${DJANGO_SUPERUSER_PASSWORD:-}" ]; then
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ['DJANGO_SUPERUSER_USERNAME']
+email = os.environ['DJANGO_SUPERUSER_EMAIL']
+password = os.environ['DJANGO_SUPERUSER_PASSWORD']
+
+user, created = User.objects.get_or_create(
+    username=username,
+    defaults={
+        'email': email,
+        'is_staff': True,
+        'is_superuser': True,
+        'is_active': True,
+    }
+)
+
+if created:
+    user.set_password(password)
+    user.save()
+    print('Superuser created successfully.')
+else:
+    user.is_staff = True
+    user.is_superuser = True
+    user.is_active = True
+    user.save()
+    print('Superuser already exists.')
+"
+
+fi
